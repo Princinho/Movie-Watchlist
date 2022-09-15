0f30917eb01a42db782bd243e037120f8eb98146
Princinho <princegnakou@gmail.com>
2022-09-15 23:16:51 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+//https://www.omdbapi.com/?s=blade&apikey=24da6167
+const apikey='24da6167'
+const searchInput=document.getElementById('search-input')
+const searchButton=document.getElementById('btn-search')
+const films=[]
+searchInput.addEventListener('keypress',(ev)=>{
+    if(ev.key=='Enter'){
+        console.log('Search for '+searchInput.value)
+        getFilms(searchInput.value)
+    }
+})
+searchButton.addEventListener('click',()=>getFilms(searchInput.value))
+function getFilms(searchQuery){
+    fetch(`https://www.omdbapi.com/?s=${searchQuery}&apikey=${apikey}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        displayFilms(data.Search)
+    })
+    .catch(err=>{
+        document.getElementById('not-found-text').style.display='block'
+        document.getElementById('placeholder').style.display='none'
+    })
+}
+function displayFilms(films){
+    const filmIds=films.map(f=>f.imdbID)
+    const requests=filmIds.map(id=>fetch(`http://www.omdbapi.com/?i=${id}&apikey=${apikey}`))
+    Promise.all(requests).then(
+        responses=>{
+            Promise.all(responses.map(r=>r.json()))
+            .then(data=>{
+                console.log(data)
+                document.getElementById('main')
+                .innerHTML=getFilmsHtml(data)
+            })
+        })
+        .catch(err=>{
+            console.error(err)
+        })
+}
+
+function addToWatchList(movieId,target){
+    if(localStorage.getItem('watchlist')){
+        let previous=JSON.parse(localStorage.getItem('watchlist'))
+        
+        localStorage.setItem('watchlist',JSON.stringify([...previous,movieId]))
+    }else{
+        localStorage.setItem('watchlist',JSON.stringify([movieId]))
+    }
+    target.style.display='none'
+}
+
+function isFavorite(filmId){
+    if(!localStorage.getItem('watchlist'))return false
+    return JSON.parse(localStorage.getItem('watchlist')).some(e=>e==filmId)
+
+}
+
+function getFilmsHtml(films){
+    let html=``
+
+    for(let film of films){
+        const button=isFavorite(film.imdbID)?'':
+        `<button class="btn-action" onClick="addToWatchList('${film.imdbID}',this)">
+        <img src="./img/add.png">
+        Watchlist
+    </button>`
+        html+=`<div class="movie" data-movie-id="${film.imdbID}">
+        <img class="movie-poster" src="${film.Poster}"/>
+        <div class="movie-details-container">
+            <div class="movie-title-bar">
+                <h3 class="movie-title">${film.Title}</h3>
+                <img src="./img/star.png">
+                <span class="movie-rating">${film.imdbRating}</span>
+            </div>
+            <div class="movie-info-bar">
+                <span class="movie-duration">${film.Runtime}</span>
+                <span class="movie-tags">${film.Genre}</span>
+                ${button}
+            </div>
+            <p class="synopsis">
+                ${film.Plot}...<span class="read-more">Read more</span>
+            </p>
+        </div>
+    </div>`
+    }
+    return html
+}
---
+++ watchlist.js
@@ -0,0 +1,93 @@
+
+const apikey='24da6167'
+let films=[]
+const mainDefaultHtml=`
+<div class="placeholder" id="placeholder">
+    <h3>Your watchlist is looking a little empty...</h3>
+    <a href="index.html" class="action-link">
+        <img src="img/add.png" class="action-img" alt="plus-button">
+        Let's add some movies!
+    </a>
+</div>
+<div id="not-found-text" class="placeholder">
+    <h3>
+        Unable to find what you're looking for. Please try another search.
+    </h3>
+</div>`
+
+const main=document.getElementById('main')
+if(localStorage.getItem('watchlist')){
+    films=JSON.parse(localStorage.getItem('watchlist'))
+    if(films.length>0)
+    displayFilms(films)
+}else{
+    console.log('no films in watchlist')
+}
+
+function displayFilms(films){
+    const requests=films.map(id=>fetch(`http://www.omdbapi.com/?i=${id}&apikey=${apikey}`))
+    Promise.all(requests).then(
+        responses=>{
+            console.log(responses)
+            Promise.all(responses.map(r=>r.json()))
+            .then(data=>{
+                console.log(data)
+                document.getElementById('main')
+                .innerHTML=getFilmsHtmlWatchlist(data)
+            })
+        })
+        .catch(err=>{
+            console.error(err)
+        })
+}
+
+function isFavorite(filmId){
+    if(!localStorage.getItem('watchlist'))return false
+    return JSON.parse(localStorage.getItem('watchlist')).some(e=>e==filmId)
+
+}
+function getFilmsHtmlWatchlist(films){
+    let html=``
+
+    for(let film of films){
+        const button= `<button class="btn-action" onClick="removeFromWatchlist('${film.imdbID}',this)">
+        <img src="./img/remove.png">
+        Watchlist
+    </button>`
+        html+=`<div class="movie" data-movie-id="${film.imdbID}">
+        <img class="movie-poster" src="${film.Poster}"/>
+        <div class="movie-details-container">
+            <div class="movie-title-bar">
+                <h3 class="movie-title">${film.Title}</h3>
+                <img src="./img/star.png">
+                <span class="movie-rating">${film.imdbRating}</span>
+            </div>
+            <div class="movie-info-bar">
+                <span class="movie-duration">${film.Runtime}</span>
+                <span class="movie-tags">${film.Genre}</span>
+                ${button}
+            </div>
+            <p class="synopsis">
+                ${film.Plot}...<span class="read-more">Read more</span>
+            </p>
+        </div>
+    </div>`
+    }
+    return html
+}
+
+
+function removeFromWatchlist(movieId,target){
+    if(localStorage.getItem('watchlist')){
+        let previous=JSON.parse(localStorage.getItem('watchlist'))
+        const newWatchlist=previous.filter(id=>id!==movieId)
+        if(newWatchlist.length==0) {
+            document.getElementById('main').innerHTML=mainDefaultHtml
+            localStorage.removeItem("watchlist")
+        }
+        localStorage.setItem('watchlist',JSON.stringify(newWatchlist))
+    }else{
+        localStorage.setItem('watchlist',JSON.stringify([movieId]))
+    }
+    target.parentNode.parentNode.parentNode.style.display='none'
+}
